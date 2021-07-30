b9771cb032885433b95372eb1f4bc7eb8645f430
Vijay <Vijay@Vijay-PC>
2021-07-30 17:50:36 +0530

    done the said changes

--- MyAmazonCucumber/src/main/java/com/qa/pages/QuotePage.java
+++ MyAmazonCucumber/src/main/java/com/qa/pages/QuotePage.java
@@ -1,5 +1,10 @@
 package com.qa.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class QuotePage {
+	
+	WebDriver driver= new ChromeDriver();
 
 }
